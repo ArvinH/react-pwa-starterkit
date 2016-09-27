5e06d9dff275e87842d4dd6f4fe1ddbf82c38507
arvinh <arvin0731@gmail.com>
2016-09-27 12:10:17 +0800

    add css support

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import './HomePage.css';
 
 const HomePage = ({
   userId,
   onSubmitUserId,
   onChangeUserId,
 }) => (
-  <div>
+  <div className="Ta(c) M(20px)">
     <TextField
       hintText="Please Key in your Github User Id."
       onChange={onChangeUserId}
@@ -25,8 +26,8 @@ const HomePage = ({
 );
 
 HomePage.propTypes = {
-  onSubmitUserId: React.PropTypes.Object,
-  onChangeUserId: React.PropTypes.Object,
+  onSubmitUserId: React.PropTypes.func,
+  onChangeUserId: React.PropTypes.func,
   userId: React.PropTypes.string,
 };
 
