6362042fc1809f692af3261847073e6a83143908
arvinh <arvin0731@gmail.com>
2016-09-28 12:58:18 +0800

    update webpack.config.prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -26,7 +26,7 @@ module.exports = {
       sourceMap: false,
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': 'production',
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new SWPrecacheWebpackPlugin(
       {
