966e32d05fb6a624cf24872ae60d1bf645a60ea7
arvinh <arvin0731@gmail.com>
2017-06-02 00:32:44 +0800

    use babel-core for server.js

--- RunServer.js
+++ RunServer.js
@@ -1,38 +1,11 @@
-const path = require('path');
-const express = require('express');
-const webpack = require('webpack');
-const config = require('./webpack.config');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-// const handleServerRender = require('./server/handleServerRender');
-
-const app = express();
-const compiler = webpack(config);
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
-// uncomment after placing your favicon in /public
-// app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'statics')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+require('babel-core/register')({
+  presets: [
+    'es2015',
+    'stage-1',
+    'react',
+  ],
 });
 
-// app.use(handleServerRender);
+require('babel-polyfill');
 
-app.listen(3000, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.log('Listening at http://localhost:3000');
-});
+module.exports = require('./server');
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('../webpack.config');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const handleServerRender = require('./handleServerRender');
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+// uncomment after placing your favicon in /public
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'statics')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(handleServerRender);
+
+app.listen(3000, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
+
+module.exports = app;
