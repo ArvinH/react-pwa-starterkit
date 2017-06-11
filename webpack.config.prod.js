efa6de00a1346f89f15f7249d6d598a62819fe2a
arvinh <arvin0731@gmail.com>
2017-06-11 19:42:48 +0800

    add public path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.[chunkhash].js',
+    publicPath: '/statics',
   },
   plugins: [
     new CleanWebpackPlugin(['dist'], { verbose: false }),
